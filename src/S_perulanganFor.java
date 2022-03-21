9650ca7bd2e695b6c9fee2f687a87ce7d4f4f910
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:59:15 +0700

    percobaan 19 menggunakan perulangan for

---
+++ src/S_perulanganFor.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class S_perulanganFor {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukkan jumlah perulangan:");
+        int jml = input.nextInt();
+        for (int i = 0; i < jml; i++)
+            System.out.println("Pemrograman Berorientasi Objek dengan Java!");
+
+    }
+}
