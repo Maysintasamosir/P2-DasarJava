15d41b6ee09fb5bdf198b31fa5df03e8e5155ec3
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:33:03 +0700

    percobaan 12 menggunakan operatot kondsisi

---
+++ src/L_Conditional.java
@@ -0,0 +1,9 @@
+public class L_Conditional {
+    public static void main(String[] args) {
+        //menggunakan operatot kondsisi
+        int x=0;
+        boolean isEven=false;
+        x=isEven?4:7;
+        System.out.println("x="+x);
+    }
+}
