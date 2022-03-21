4683e2d30001f6cfbab5ccfb71dc575e54d20c8b
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:14:52 +0700

    percobaan 1 mengamati hasil perubahan nilai dari suatu operasi metematis

---
+++ src/A_OperatorA.java
@@ -0,0 +1,13 @@
+public class A_OperatorA {
+    public static void main(String[] args) {
+        // mengamati hasil perubahan nilai dari suatu operasi metematis
+        float number1 =15;
+        float number2 =6;
+        float result = number1 / number2;
+        float remainder = number1 % number2;
+        System.out.println(number1 +"divided by"+number2);
+        System.out.println("\nResult\tRemainder");
+        System.out.println(result+"\t"+remainder);
+
+    }
+}
