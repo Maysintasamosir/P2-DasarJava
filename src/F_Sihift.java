20c2afbc29ad5659269ce71fb633d72771abb2fd
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:26:55 +0700

    percobaan 6 melakukan operasi shift

---
+++ src/F_Sihift.java
@@ -0,0 +1,13 @@
+public class F_Sihift {
+    public static void main(String[] args) {
+        //melakukan operasi shift
+        int x=7;
+        System.out.println("x="+x);
+        System.out.println("x>>2="+(x>>2));
+        System.out.println("x<<1="+(x<<1));
+        System.out.println("x>>>1="+(x>>>2));
+
+
+
+    }
+}
