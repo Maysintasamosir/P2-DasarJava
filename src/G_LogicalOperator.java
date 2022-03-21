abdb0a3c3c9fe47b792f55e27f6dc52178a7edf4
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:28:02 +0700

    percobaan 7 menggunakan operator logika

---
+++ src/G_LogicalOperator.java
@@ -0,0 +1,16 @@
+public class G_LogicalOperator {
+    public static void main(String[] args) {
+        //menggunakan operator logika
+        int x =7,y=11,z=11;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+        System.out.println("x<y="+(x<y));
+        System.out.println("x>z="+(x>z));
+        System.out.println("y<=z="+(x<=z));
+        System.out.println("x<=y="+(x>=y));
+        System.out.println("y==z="+(x==z));
+        System.out.println("x!=y="+(x!=z));
+
+    }
+}
