dc152485452ef95fa9080e7c9d1b369907e0e307
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:29:27 +0700

    percobaan 8 menggunakan operator boolean and

---
+++ src/H_BooleanAnd.java
@@ -0,0 +1,11 @@
+public class H_BooleanAnd {
+    public static void main(String[] args) {
+        //menggunakan operator boolean and
+        int a=5,b=7;
+        if ((a<2)&(b++<10)) {
+            b += 2;
+        }
+            System.out.println(b);
+
+    }
+}
