769803f43766e8e06b27f13fc34cf06fa0de6f14
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:34:44 +0700

    percobaan 13 menggunakan percabangan if,if-else,dan

---
+++ src/M_PercabanganIF.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class M_PercabanganIF {
+    public static void main(String[] args) {
+        //menggunakan percabangan if,if-else,dan if-else if
+
+        //if
+        if (15>5){
+            System.out.println("true");
+        }
+        System.out.println("the program continues here....");
+        //if-else
+        int a =5;
+        int b=2;
+        int max;
+        if (a>b) {
+            max = a;
+        }else {
+            max=b;
+        }
+        System.out.println("maximum of a or b is"+max);
+        //if-else if
+        int myVar=0;//the variabel is initialized with a value of 0 myVar=0
+        if (myVar==0) {//if the value is 0,we change its value to
+            myVar = 1;
+        }else if(myVar==1) {//if the value is 1,we change its value to 0
+            myVar = 0;
+        }else{
+            myVar=-1;
+        }
+    }
+}
