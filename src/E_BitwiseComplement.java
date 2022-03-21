1474870db7a7295a05a402f68236dfed09b9d81b
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:25:42 +0700

    percobaan 5 melakukan operasi kemplemen

---
+++ src/E_BitwiseComplement.java
@@ -0,0 +1,9 @@
+public class E_BitwiseComplement {
+    public static void main(String[] args) {
+        //melakukan operasi kemplemen
+        int x=8;
+        System.out.println("x="+x);
+        int y=~x;
+        System.out.println("y="+y);
+    }
+}
