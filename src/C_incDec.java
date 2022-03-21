21ff0be85cd1cd1ffe85fa5e0aeb957e225e6d31
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:21:49 +0700

    percobaan 3 melakukan increment dan decremen nilai

---
+++ src/C_incDec.java
@@ -0,0 +1,18 @@
+public class C_incDec {
+    public static void main(String[] args) {
+        //melakukan increment dan decremen nilai
+        int x=8,y=13;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("y="+y++);
+        System.out.println("x="+x--);
+        System.out.println("y="+--y);
+
+
+
+
+
+
+
+    }
+}
