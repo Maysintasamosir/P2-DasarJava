33c12371f5d1c4a67842648fb5e525f721fdf45f
Maysintasamosir <Maysintasamosir@gmail.com>
2022-03-21 14:31:23 +0700

    percobaan 10 menggunakan boolean or

---
+++ src/J_BooleanOr.java
@@ -0,0 +1,10 @@
+public class J_BooleanOr {
+    public static void main(String[] args) {
+        //menggunakan boolean or
+        int a=5,b=7;
+        if ((a>2)|(b++<10)){
+            b+=2;
+        }
+        System.out.println(b);
+    }
+}
